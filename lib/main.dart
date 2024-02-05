294f3c80ee3e4aa898f28518bd68185de64ceea6
alibaransel <alibaransel25@gmail.com>
2024-02-05 21:04:27 +0300

    Add main structure steps as comment line

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,10 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  // Create data (optional, for problems can be solved with formula or algorithm)
+  // Read data
+  // Shuffle data
+  // Split data to train and test parts
+  // Build neural network
+  // Train network
+  // Test data
+  // Calculate and print accuracy
+}
