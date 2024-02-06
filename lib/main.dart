7550ad8b8a9d3a3867522f2465b004388b32a24e
alibaransel <alibaransel25@gmail.com>
2024-02-06 04:16:05 +0300

    Implement data and network build

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+import 'neural.dart';
+
 void main(List<String> args) {
   // Create data (optional, for problems can be solved with formula or algorithm)
   // Read data
@@ -7,4 +10,13 @@ void main(List<String> args) {
   // Train network
   // Test data
   // Calculate and print accuracy
+
+  List<List<int>> data = [];
+  for (int i = 0; i < 10000; i++) {
+    int firstBit = Random().nextInt(2);
+    int secondBit = Random().nextInt(2);
+    int result = firstBit & secondBit;
+    data.add([firstBit, secondBit, result]);
+  }
+  Network network = Network.build([2, 10, 2]);
 }
